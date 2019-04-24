320cd1d65d35ebaa63c0144f4604cf17ca5cc2ae
Gabriele Tolomei <gabriele.tolomei@gmail.com>
2019-04-24 16:14:14 +0200

    Script for generating spatial index

--- multiclass/generate_spatial_index.py
+++ multiclass/generate_spatial_index.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 """
-generate_kd_trees.py
+generate_spatial_index.py
 
 Created by Gabriele Tolomei on 2019-04-24.
 """
@@ -16,6 +16,9 @@ import numpy as np
 
 from sklearn.externals import joblib
 from sklearn.neighbors import KDTree
+from sklearn.neighbors import BallTree
+
+from functools import partial
 
 
 def configure_logging(level=logging.INFO):
@@ -36,7 +39,7 @@ def configure_logging(level=logging.INFO):
 
     # log to file
     file_handler = logging.FileHandler(
-        filename="./generate_kd_trees.log", mode="w")
+        filename="./generate_spatial_index.log", mode="w")
     file_handler.setLevel(level)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
@@ -50,20 +53,29 @@ def get_options(cmd_args=None):
     """
     cmd_parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description="""Generate a KD tree data structure from all the k-labelled transformation (i.e., one KD tree for each class label k).""")
+        description="""Generate a spatial index data structure from all the k-labelled transformation (i.e., one index for each class label k).""")
     cmd_parser.add_argument(
         'transformations_filename',
         help="""Path to the file containing the serialized transformations.""",
         type=str)
     cmd_parser.add_argument(
         'output_filename',
-        help="""Path to the output file, which will contain the KD tree index.""",
+        help="""Path to the output file, which will contain the spatial index.""",
         type=str)
+    cmd_parser.add_argument(
+        '-t',
+        '--type',
+        default='kd-tree',
+        const='kd-tree',
+        nargs='?',
+        choices=['kd-tree', 'ball-tree'],
+        help="""Type of spatial index to be generated (default: %(default)s)""")
     args = cmd_parser.parse_args(cmd_args)
 
     options = {}
     options['transformations_filename'] = args.transformations_filename
     options['output_filename'] = args.output_filename
+    options['type'] = args.type
 
     return options
 
@@ -73,21 +85,21 @@ def load_transformations(transformations_filename):
         return joblib.load(input_file)
 
 
-def save_kd_trees(kd_trees, output_filename):
+def save_spatial_indices(index, output_filename):
     with gzip.GzipFile(output_filename + '.gz', 'wb') as output_file:
-        joblib.dump(kd_trees, output_file)
+        joblib.dump(index, output_file)
 
 
-def map_compute_kd_trees(instance):
+def map_compute_spatial_index(instance, index_type=KDTree):
 
     logger = logging.getLogger(__name__)
 
     label, X = instance
 
     logger.info(
-        "Computing KD-trees for all classes with label k = {}".format(label))
+        "Computing {} for all transformations with label k = {}".format(index_type.__name__, label))
 
-    return ((label), KDTree(X))
+    return (label, index_type(X))
 
 
 def main(options):
@@ -104,61 +116,48 @@ def main(options):
             k, len(transformations[k])))
     logger.info("******************************************")
 
+    logger.info("==> Generating `{}` spatial index".format(options['type']))
+
+    index_type = None
+
+    if options['type'] == 'kd-tree':
+        index_type = KDTree
+    if options['type'] == 'ball-tree':
+        index_type = BallTree
+
     # Setting up multiple processes (i.e., workers)
     logger.info("==> Creating the pool of {} workers".format(mp.cpu_count()))
     pool = mp.Pool(mp.cpu_count())
     logger.info("==> Preparing the input to be sent to each worker of the pool")
 
-    # dictionary of KD-trees, where each class label is a key
-    # and a KD-tree is created using the instances (i.e., transformations) generated from the model's internals
-    kd_trees = {}
+    # dictionary of spatial indices, where each class label is a key
+    # and a tree (e.g., KDTree or BallTree) is created using the instances (i.e., transformations) generated from the model's internals
+    index = {}
     # list of inputs sent to each worker [(input_w1), ..., (input_wm)]
     inputs = []
     # loop through every label
     for label in transformations:
         logger.info(
-            "Generate KD-tree data structure from {}-labelled transformations".format(label))
+            "Generate `{}` spatial index data structure from {}-labelled transformations".format(options['type'], label))
         # check if there exists at least one valid instance
         if transformations[label] and len(transformations[label]) > 0:
             # stack (vertically) all the instances (i.e., numpy arrays) which represent k-labelled transformations
             X = np.vstack(transformations[label])
             logger.info(
-                "Number of instances to be indexed with KD-tree for label k = {}: {}".format(label, X.shape[0]))
+                "Number of instances to be indexed for class label k = {}: {}".format(label, X.shape[0]))
             # if so, just append the portion of the dataset (plus extra arguments) to the list of inputs
             inputs.append((label, X))
         else:
             logger.info(
                 "No transformations associated with label k = {}".format(label))
 
-    kd_trees = dict(pool.map(map_compute_kd_trees, inputs))
+    func = partial(map_compute_spatial_index, index_type=index_type)
+    index = dict(pool.map(func, inputs))
 
-    for k in kd_trees:
-        print(dir(kd_trees[k]))
-
-    # Extract all the (k-leaved) paths from the loaded tree ensemble
-    # logger.info("==> Extracting all paths from the just loaded model ...")
-    # paths = enumerate_paths(
-    #     model, tmp_filename=options['model_filename'] + '.paths.tmp')
-
-    # for k in model.classes_:
-    #     k_tot = 0
-    #     for t in range(len(model.estimators_)):
-    #         logger.debug(
-    #             "Number of {}-leaved paths of tree ID #{}: {}".format(k, t, len(paths[k][t])))
-    #         k_tot += len(paths[k][t])
-
-    #     logger.info("Total number of {}-leaved paths: {}".format(k, k_tot))
-
-    # if os.path.isfile(options['model_filename'] + '.paths.tmp'):
-    #     # Clear temporary path file
-    #     logger.info("==> Cleaning up temporary path file `{}`".format(
-    #         options['model_filename'] + '.paths.tmp'))
-    #     os.remove(options['model_filename'] + '.paths.tmp')
-
-    # # Save all the k-leaved paths to disk
-    # logger.info("==> Saving all the extracted paths to `{}.gz`".format(
-    #     options['output_filename']))
-    # dump_paths(paths, options['output_filename'])
+    # Save all the spatial indices
+    logger.info("==> Saving all the {} spatial index to `{}.gz`".format(
+        options['type'], options['output_filename']))
+    save_spatial_indices(index, options['output_filename'])
 
 
 if __name__ == '__main__':
